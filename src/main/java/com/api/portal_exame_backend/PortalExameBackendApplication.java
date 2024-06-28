f202edd7ccb1292c4044d9e895f4b3a59d80692b
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-28 02:02:58 -0300

    feat: criando projeto SpringBoot

---
+++ src/main/java/com/api/portal_exame_backend/PortalExameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.portal_exame_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalExameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalExameBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/portal_exame_backend/PortalExameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.portal_exame_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalExameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
