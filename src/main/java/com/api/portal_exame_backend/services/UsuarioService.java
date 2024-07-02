51b45d0f1982d3475ddfa5e92a7344500ee46756
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-01 22:05:48 -0300

    feat: add o Service Usuario  e criando método criarUsuario( )

---
+++ src/main/java/com/api/portal_exame_backend/services/UsuarioService.java
@@ -0,0 +1,11 @@
+package com.api.portal_exame_backend.services;
+
+import java.util.Set;
+
+import com.api.portal_exame_backend.model.Usuario;
+import com.api.portal_exame_backend.model.UsuarioRole;
+
+public interface UsuarioService {
+    
+    public Usuario criarUsuario(Usuario usuario, Set<UsuarioRole> usuarioRoles) throws Exception;
+}
---
+++ src/main/java/com/api/portal_exame_backend/services/impl/UsuarioServiceImpl.java
@@ -0,0 +1,40 @@
+package com.api.portal_exame_backend.services.impl;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.portal_exame_backend.model.Usuario;
+import com.api.portal_exame_backend.model.UsuarioRole;
+import com.api.portal_exame_backend.repositories.RoleRepository;
+import com.api.portal_exame_backend.repositories.UsuarioRepository;
+import com.api.portal_exame_backend.services.UsuarioService;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public Usuario criarUsuario(Usuario usuario, Set<UsuarioRole> usuarioRoles) throws Exception {
+        
+        Usuario novoUsuario = usuarioRepository.findbyUsername(usuario.getUsername());
+        if (novoUsuario != null) {
+            System.out.println("Usuário já existe");
+            throw new Exception("Usuário já existe");
+        } else {
+            for (UsuarioRole usuRole : usuarioRoles) {
+                roleRepository.save(usuRole.getRole());
+            }
+            usuario.getUsuarioRoles().addAll(usuarioRoles);
+            novoUsuario = usuarioRepository.save(usuario);
+        }
+        return novoUsuario;
+    }
+
+}
